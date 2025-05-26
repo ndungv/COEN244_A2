928409aa956aec21042b03648d763e9cd557199f
RI-D <58811911+RI-D@users.noreply.github.com>
2025-05-26 08:32:07 -0400

    Update Date.h

--- Date.h
+++ Date.h
@@ -7,8 +7,11 @@ using namespace std;
 class Date {
 public:
     Date();
+    Date(int day, month, year);
+
 //destruction
     ~Date();
+
 //Getter and setter 
     int getDay() const;
     void setDay(int day);
@@ -16,7 +19,6 @@ public:
     void setMonth(int month);
     int getYear() const;
     void setYear(int year);
-
 
 private:
     int year;
