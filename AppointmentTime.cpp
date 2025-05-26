5a7ab6b2fefd5770983d1cc4b2bfced2920b32fe
RI-D <58811911+RI-D@users.noreply.github.com>
2025-05-26 09:12:58 -0400

    Update AppointmentTime.cpp

--- AppointmentTime.cpp
+++ AppointmentTime.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
 #include "AppointmentTime.h"
 using namespace std;
+
+//again, I don't think hour and minute should be negative, maybe I am wrong, idk
+/*
 AppointmentTime::AppointmentTime(){
     day = "";
   hour = -1;
   minute = -1;
 }
+*/
+
+//Default
+AppointmentTime::AppointmentTime(){
+    day = "";
+    hour = 0;
+    minute = 0;
+}
+
 AppointmentTime::AppointmentTime(string d, int h, int m){
   day = d;
   hour = h;
   minute = m;
+}
+//Destruction 
+AppointmentTime::~AppointmentTime() {
+    cout << "AppointmentTime: destroyed" << endl;
+}
+//Getter and Setters 
+string AppointmentTime::getDay() const {
+ return day; 
+}
+void AppointmentTime::setDay(string d) {
+ day = d; 
+}
+int AppointmentTime::getHour() const {
+ return hour; 
+}
+void AppointmentTime::setHour(int h) {
+ hour = h; 
+}
+int AppointmentTime::getMinute() const {
+ return minute; 
+}
+void AppointmentTime::setMinute(int m) {
+ minute = m; 
 }
