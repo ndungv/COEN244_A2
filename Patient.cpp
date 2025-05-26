eca8020ff143fbe5f66307c9c690e9ba3f262921
RI-D <58811911+RI-D@users.noreply.github.com>
2025-05-26 15:55:55 -0400

    Update Patient.cpp

--- Patient.cpp
+++ Patient.cpp
@@ -13,7 +13,7 @@ Patient::Patient(string p_name, string insurance, string d_name) {
     patient_name = p_name;;
     patient_insurance = insurance;
     doctor_name = d_name;
-}
+} 
 
 void Patient::print_patient_info() {
     cout<<"Patient Name: "<<patient_name<<endl;
