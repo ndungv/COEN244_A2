50e5e59a34966cf26741d7a9f4bd0f8de1fc5232
RI-D <58811911+RI-D@users.noreply.github.com>
2025-05-26 09:01:27 -0400

    Update AppointmentTime.h

--- AppointmentTime.h
+++ AppointmentTime.h
@@ -5,9 +5,20 @@
 using namespace std;
 
 class AppointmentTime{
-    public:
+public:
     AppointmentTime();
     AppointmentTime(string d, int h, int m);
+//Destruction
+    ~AppointmentTime();
+
+//Getter and setter
+    string getDay() const;
+    void setDay(string d);
+    int getHour() const;
+    void setHour(int h);
+    int getMinute() const;
+    void setMinute(int m);
+
 private:
     string day;
     int hour;
